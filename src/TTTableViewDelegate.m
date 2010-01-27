8f23495c69b3c0b6e178b5298523a661efa7b98d
uprise78 <desaro@gmail.com>
2010-01-26 18:37:34 -0800

    if a TTTableItem has a userInfo object pass it along automatically when opening the items URL

--- src/TTTableViewDelegate.m
+++ src/TTTableViewDelegate.m
@@ -80,28 +80,56 @@ static const CGFloat kSectionHeaderHeight = 35;
   return nil;
 }
 
-- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
-  id<TTTableViewDataSource> dataSource = (id<TTTableViewDataSource>)tableView.dataSource;
-  id object = [dataSource tableView:tableView objectForRowAtIndexPath:indexPath];
-  if ([object isKindOfClass:[TTTableLinkedItem class]]) {
-    TTTableLinkedItem* item = object;
-    if (item.URL && [_controller shouldOpenURL:item.URL]) {
-      TTOpenURL(item.URL);
-    }
+- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
+{
+	id<TTTableViewDataSource> dataSource = (id<TTTableViewDataSource>)tableView.dataSource;
+	id object = [dataSource tableView:tableView objectForRowAtIndexPath:indexPath];
+	
+	if( [object isKindOfClass:[TTTableLinkedItem class]] )
+	{
+		TTTableLinkedItem* item = object;
+			
+		if( item.URL && [_controller shouldOpenURL:item.URL] )
+		{
+			// If the TTTableItem has userInfo, wrap it up and send it along to the URL
+			if( item.userInfo )
+			{
+				NSDictionary *userInfoDict;
+				
+				// If userInfo is a dictionary, pass it along else create a dictionary
+				if( [item.userInfo isKindOfClass:[NSDictionary class]] )
+				{
+					userInfoDict = item.userInfo;
+				}
+				else
+				{
+					userInfoDict = [NSDictionary dictionaryWithObject:item.userInfo forKey:@"__userInfo__"];
+				}
 
-    if ([object isKindOfClass:[TTTableButton class]]) {
-      [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    } else if ([object isKindOfClass:[TTTableMoreButton class]]) {
-      TTTableMoreButton* moreLink = (TTTableMoreButton*)object;
-      moreLink.isLoading = YES;
-      TTTableMoreButtonCell* cell
-        = (TTTableMoreButtonCell*)[tableView cellForRowAtIndexPath:indexPath];
-      cell.animating = YES;
-      [tableView deselectRowAtIndexPath:indexPath animated:YES];
-      
-      [_controller.model load:TTURLRequestCachePolicyDefault more:YES];
-    }
-  }
+				[[TTNavigator navigator] openURL:item.URL query:userInfoDict animated:YES];
+			}
+			else
+			{
+				TTOpenURL( item.URL );
+			}
+		}
+
+
+		if ([object isKindOfClass:[TTTableButton class]])
+		{
+			[tableView deselectRowAtIndexPath:indexPath animated:YES];
+		}
+		else if ([object isKindOfClass:[TTTableMoreButton class]])
+		{
+			TTTableMoreButton* moreLink = (TTTableMoreButton*)object;
+			moreLink.isLoading = YES;
+			TTTableMoreButtonCell* cell	= (TTTableMoreButtonCell*)[tableView cellForRowAtIndexPath:indexPath];
+			cell.animating = YES;
+			[tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+			[_controller.model load:TTURLRequestCachePolicyDefault more:YES];
+		}
+	}
 
   [_controller didSelectObject:object atIndexPath:indexPath];
 }
